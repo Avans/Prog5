b0f6400a48c66bb025c77fef50420fe77bd7f90c
Stino <Linksonder@gmail.com>
2015-10-15 18:32:06 +0200

    GH en CD

---
+++ Prog5_Uitwerkingen_Week6/Dierentuin_GH/Model/DierContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dierentuin_GH.Model
+{
+    public class DierContext : DbContext
+    {
+        public DierContext()
+            : base("name=DierContext")
+        {
+ 
+        }
+   
+        public DbSet<Dier> Dieren { get; set; }
+    }
+}
---
+++ Prog5_Uitwerkingen_Week6/Dierentuin_GH/ViewModel/MainVM.cs
@@ -0,0 +1,65 @@
+﻿using Dierentuin_GH.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dierentuin_GH.ViewModel
+{
+    public class MainVM : ViewModelBase
+    {
+        public ObservableCollection<DierVM> Dieren { get; set; }
+
+        public DierVM SelectedDier { get; set; }
+
+        public ShowDetailsCommand ShowDetailsCommand { get; set; }
+
+        public ICommand ShowAddDierCommand {get; set; }
+        public ICommand SaveDierCommand { get; set; }
+
+        DierContext context;
+
+        public MainVM()
+        {
+            context = new DierContext();
+
+            //1. Ophalen data uit databse
+            IEnumerable<Dier> dieren = context.Dieren;
+
+            //2. De Data omtoveren naar een VM
+            IEnumerable<DierVM> dierenVM =  dieren.Select(dier => new DierVM(dier));
+
+            //3. De data beschikbaar maken via een property
+            Dieren = new ObservableCollection<DierVM>(dierenVM);
+
+            ShowDetailsCommand = new ShowDetailsCommand();
+            ShowAddDierCommand = new RelayCommand(ShowDetailsWindow);
+            SaveDierCommand = new RelayCommand(SaveDier);
+        }
+
+        AddDier window;
+
+        private void SaveDier()
+        {
+            Dier dier = SelectedDier.ToPOCO();
+            dier = context.Dieren.Add(dier);
+            context.SaveChanges();
+            //Ik wil een nieuw VM omdat ik graag het nieuwe ID wil hebben
+            Dieren.Add(new DierVM(dier));
+            window.Hide();
+        }
+
+        public void ShowDetailsWindow()
+        {
+            window = new AddDier();
+            SelectedDier = new DierVM();
+            RaisePropertyChanged("SelectedDier");
+            window.Show();
+        }
+    }
+}
