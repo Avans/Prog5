296fc4e979cf4b40b23c5b22ce00bfa5a2b44896
Stino <Linksonder@gmail.com>
2015-10-17 17:42:27 +0200

    Groep A & B

---
+++ Prog5_Uitwerkingen_Week6/Kapsalon_AB/Model/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Kapsalon_AB.Model
+{
+    [Table("Ingredient")]
+    public class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String Naam { get; set; }
+
+        public double Prijs { get; set; }
+    }
+}
