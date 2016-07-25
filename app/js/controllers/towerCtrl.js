258f88293a3570d164f07155657a50528d65521c
Stino <Linksonder@gmail.com>
2016-07-25 18:23:11 +0200

    stijl voor mobile

--- app/js/controllers/towerCtrl.js
+++ app/js/controllers/towerCtrl.js
@@ -1,19 +1,23 @@
-module.exports = function ($scope, $timeout, $mdSidenav, $log, $rootScope) {
-    
+module.exports = function ($scope, $timeout, $mdSidenav, $log, $rootScope, $q) {
+    var self = this;
+
 	var ratio = 1.743;
-	
+
+	self.imageLocations = [
+		{ src: "default/img/Background_medium.png", offset: 0.4 },
+		{ src: "default/img/wolk1_medium.png", offset: 0.5, classes:"cloud" },
+		{ src: "default/img/MVC_medium.png", offset: 0 },
+		{ src: "default/img/WCF_medium.png", offset: 1.2 },
+		{ src: "default/img/EF.png", offset: 1.4 },
+		{ src: "default/img/WPF.png", offset: 1.6 },
+		{ src: "default/img/wolk2_medium.png", offset: 0, classes:"cloud reeverse" },
+	];
+
 	var canvasWidth = document.getElementById('canvas').offsetWidth;
 	var canvasHeight = document.getElementById('canvas').offsetHeight;
 	
 	$scope.ready = false;
 	$scope.realHeight = (canvasWidth * ratio - canvasHeight);
-
-	$scope.loadingComplete = function(){
-		$('#loader').height(0);
-		$scope.ready = true;
-		$scope.toLevel(100);
-	}
-	
 
 	$scope.toLevel = function(newLevel){
 		$scope.level = $scope.realHeight / 100 * newLevel;
@@ -32,4 +36,27 @@ module.exports = function ($scope, $timeout, $mdSidenav, $log, $rootScope) {
 		var root = "default/img/";
 		return root + img + "_small.png 600w, " + root + img + "_medium.png 900w, " + root + img + "_large.png 1200w";
 	}
+
+	var preloadImages = function(images){
+		  var deferred = $q.defer();
+		  var count = 0;
+
+		  images.forEach(function(img){
+			$('<img>').attr({ src: img.src }).load(function() {
+				count++;
+				if(count == images.length){
+					deferred.resolve();
+				}
+			});
+		  });
+		 
+		  return deferred.promise;
+	}
+
+	preloadImages(self.imageLocations).then(function(){
+		$('#loader').height(0);
+		$scope.ready = true;
+		$scope.toLevel(100);
+	});
+	
 };
